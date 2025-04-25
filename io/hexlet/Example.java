b20b2b412bc2284daac55a029bd1681b52ffa136
egor_semichev <thebestists@gmail.com>
2025-04-25 22:54:51 +0300

    4th Lesson

---
+++ io/hexlet/Example.java
@@ -0,0 +1,10 @@
+package io.hexlet;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Exemple {
+	public static void main(String[] args) {
+		String capitalizedWord = StringUtils.capitalize("hexlet");
+		System.out.println(capitalizedWord);
+	}
+}
--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,7 +1,10 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
 	public static void main(String[] args) {
-		Two.greeting();
+		String reversed = StringUtils.reverse("Hexlet");
+		System.out.println(reversed);
 	}
 }
